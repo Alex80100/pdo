bd33a752a02aebaa9cebb6759c6aa72c287355a0
Alex80100 <flamentalexandre20@gmail.com>
2023-05-31 11:09:25 +0200

    Formulaire sécurisé

--- views/patients/adPatient.php
+++ views/patients/adPatient.php
@@ -4,6 +4,7 @@
             <h1>Formulaire d'inscription</h1>
         </div>
         <form method="POST" enctype="multipart/form-data" autocomplete="on" novalidate>
+            <!-- ========== LASTNAME ========== -->
             <div class="mb-3">
                 <label for="lastname" class="form-label">Nom de Famille : </label>
                 <input type="text" 
@@ -11,8 +12,10 @@
                 name="lastname"
                 required 
                 pattern=<?php echo $regex['regexName'] ?>
-                >
+                placeholder="Nom de Famille">
+                <small class="form-text error text-danger"><?= $error['lastname'] ?? '' ?></small>
             </div>
+            <!-- ========== FIRSTNAME ========== -->
             <div class="mb-3">
                 <label for="firstname" class="form-label">Prénom : </label>
                 <input type="text"
@@ -20,33 +23,43 @@
                 name ="firstname"
                 required 
                 pattern=<?php echo $regex['regexName'] ?>
-                >
+                placeholder="Prénom">
+                <small class="form-text error text-danger"><?= $error['firstname'] ?? '' ?></small>
             </div>
+            <!-- ========== BIRTHDATE ========== -->
             <div class="mb-3">
                 <label for="birthdate" class="form-label">Date de Naissance : </label>
                 <input type="date" 
                 class="form-control" 
                 name="birthdate" 
-                required 
-                >
+                required>
+                <small class="form-text error text-danger"><?= $error['birthdate'] ?? '' ?></small>
+
             </div>
+            <!-- ========== PHONE ========== -->
             <div class="mb-3">
                 <label for="phone" class="form-label">Téléphone : </label>
                 <input type="text" 
                 class="form-control" 
                 name="phone"
+                pattern= <?= $regex['regexPhone'] ?>
                 required 
-                >
+                placeholder="Numéro de téléphone">
+                <small class="form-text error text-danger"><?= $error['phone'] ?? '' ?></small>
             </div>
+            <!-- ========== EMAIL ========== -->
             <div class="mb-3">
                 <label for="email" class="form-label">Email : </label>
                 <input type="text" 
                 class="form-control" 
                 name="email"
-                required 
-                pattern= <?= $regex['regexPhone'] ?>>
+                required
+                placeholder="Adresse Email">
+                <small class="form-text error text-danger"><?= $error['email'] ?? '' ?></small>
             </div>
-            <button type="submit" class="btn btn-primary">Submit</button>
+            <div class="d-flex justify-content-center">
+                <button type="submit" class="btn btn-primary border-0 bg-success col-6">ENVOYER</button>
+            </div>
         </form>
     </div>
 </div>
